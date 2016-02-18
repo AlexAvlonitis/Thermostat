9b9869932d29547943d500609b10b8ca111d00f1
Alex Avlonitis <avlonitis@msn.com>
2016-02-18 16:06:43 +0000

    finished

--- src/jquery.js
+++ src/jquery.js
@@ -34,11 +34,23 @@ $(document).ready(function() {
     $(".psm").addClass("led-red");
   });
 
+
+
+  $(".api").click(function(){
+    var city = $(".city").val();
+    var apiKey = "bb6b9c3be1f0e70e4d48e1c6dd867c00";
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=";
+    $.getJSON(url + apiKey, function(data) {
+
+      $(".city-weather").html(data.name + "  " + "<img src=http://openweathermap.org/img/w/" + data.weather[0].icon + ".png>" + " " + data.main.temp + "&#8451;" );
+
+    });
+
+  });
+
   function displayDegreesChBg(){
     $(".degrees").html(thermostat.degrees + " &#8451;");
     $("#panel").css("background-color", thermostat.displayColor());
   }
-
-
 
 });
